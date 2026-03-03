a6e3f1e5dd8ba1dd0f1eaa5022d7f20fe9f6f271
Nathan-Zhao0806 <zhaon2273@my.sduhsd.net>
2026-03-03 21:52:08 +0000

    new thing

--- script.js
+++ script.js
@@ -2,12 +2,17 @@
 document.getElementById("button").addEventListener("click", () => {
     document.getElementById("para"). toggleAttribute("hidden");
 });
-document.getElementById("coolbutton").addEventListener("click", () => {
-    let cool = document.getElementById("cool");
-    let coollist =document.getElementById("coollist");
-    if(cool.value.trim() != ""){
-        const colist = document.createElement("li");
-        colist.textContent = cool.value;
-        coollist.appendChild(colist);
-        cool.value = "";
-    }});
+const btn =document.getElementById("coolbutton");
+
+if(btn) {
+    btn.addEventListener("click", () => {
+        let cool = document.getElementById("cool");
+        let coollist =document.getElementById("coollist");
+            if(cool.value.trim() != " "){
+                const colist = document.createElement("li");
+                colist.textContent = cool.value;
+                coollist.appendChild(colist);
+                cool.value = "";
+            }
+        });
+    }
