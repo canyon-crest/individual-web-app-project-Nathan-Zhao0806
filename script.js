84868c693137d6336fabec29631cd896df0c8d1d
Nathan-Zhao0806 <zhaon2273@my.sduhsd.net>
2026-03-03 21:34:14 +0000

    butttons

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // add javascript 
+document.getElementById("button").addEventListener("click", () => {
+    document.getElementById("para"). toggleAttribute("hidden");
+});
+document.getElementById("coolbutton").addEventListener("click", () => {
+    let cool = document.getElementById("cool");
+    let coollist =document.getElementById("coollist");
+    if(cool.value.trim() != ""){
+        const colist = document.createElement("li");
+        colist.textContent = cool.value;
+        coollist.appendChild(colist);
+        cool.value = "";
+    }});
